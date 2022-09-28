836a20eb1c220df170810c67667e33f807e0c843
Mansour, Raymond <58750225+Raywilleatchu@users.noreply.github.com>
2022-09-28 15:11:18 -0400

    Portfolio

--- src/js/Portfolio.js
+++ src/js/Portfolio.js
@@ -1,12 +1,15 @@
 import "../css/Portfolio.scss"
-import { useState, useEffect } from "react";
-import { Octokit } from "octokit";
+import { useState, useEffect, ReactDOM } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 
 export const Portfolio = () =>
 {
     const URL = "https://api.github.com/repos/raywilleatchu/rays-portfolio/contents/README.md"
 
     const [ doc, setDoc ] = useState([]);
+    const [ content, setContent ] = useState("");
 
     useEffect(() => {
         async function fetchData(){
@@ -20,28 +23,51 @@ export const Portfolio = () =>
     }, []);
     useEffect(() => {
         if(doc.content != null){
-            console.log(atob(doc.content))
+            setContent(atob(doc.content));
+            console.log(content.length)
         }
-        else {
-            console.log("Content is null")
-        }
-    });
-    
-    
-    // async function getGitApi() {
+    }, doc.content, content.length);
 
-    //     const gitAPI = new Octokit({
-    //         auth: process.env.REACT_APP_API_KEY
-    //     });
-    //     return useState(await gitAPI.request("GET /repos/raywilleatchu/rays-portfolio"));
-    // }
-    
-    //const properties = doc.map((data) => JSON.parse(data.payload));
-
-    //console.log("Response: ", doc);
-    
     return(
-        <div>
+        <div style={{position: "absolute", padding:"10px 10px 10px 110px", fontSize: "20px", wordBreak: "break-word"}}>
+            <div style={{display: "flex", backgroundColor: "pink"}}>
+                <div name="Quality and Health" style={{flex: "2"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(11, 3350)}</ReactMarkdown>
+                </div>
+
+                <div name="Toolng" style={{flex: "1 500px"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(4617, 9142)}</ReactMarkdown>
+                </div>
+            </div>
+
+            <div style={{display: "flex", backgroundColor: "orange"}}>
+                <div name="Fullstack" style={{flex: "2"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(11309, 13415)}</ReactMarkdown>
+                </div>
+
+                <div name="Integration" style={{flex: "1 500px"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(9140, 11312)}</ReactMarkdown>
+                </div>
+            </div>
+
+            <div style={{display: "flex", backgroundColor: "pink"}}>
+                <div name="Programming Concepts" style={{flex: "2"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(3394, 4620)}</ReactMarkdown>
+                </div>
+
+                <div nam="Vision and Growth" style={{flex: "1 500px"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(13410, 15403)}</ReactMarkdown>
+                </div>
+            </div>
+            
+            <div style={{display: "flex", backgroundColor: "orange"}}>
+                <div name="Architecture" style={{flex: "2"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(15400, 18764)}</ReactMarkdown>
+                </div>
+                <div style={{flex: "1 500px"}}>
+                    <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm} linkTarget={"_blank"}>{content.slice(18760, 20000)}</ReactMarkdown>
+                </div>
+            </div>
         </div>
     );
 }
